3fc411843f1e12db87010c6118053779e24a9f3a
Yuri-ga1 <regota09@gmail.com>
2023-11-22 19:27:24 +0800

    completed tasks

--- app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
+++ app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
@@ -4,8 +4,12 @@ import android.R.attr.x
 import android.R.attr.y
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.Console
+import kotlin.random.Random
 
 
 // тип для координат
@@ -17,21 +21,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tiles[0][0] = findViewById(R.id.t00) // TODO: заполнить массив для всех тайлов
+        //tiles = Array(4) { Array(4){View(this)} }
+        tiles = arrayOf(
+            arrayOf(findViewById(R.id.t00), findViewById(R.id.t01), findViewById(R.id.t02), findViewById(R.id.t03)),
+            arrayOf(findViewById(R.id.t10), findViewById(R.id.t11), findViewById(R.id.t12), findViewById(R.id.t13)),
+            arrayOf(findViewById(R.id.t20), findViewById(R.id.t21), findViewById(R.id.t22), findViewById(R.id.t23)),
+            arrayOf(findViewById(R.id.t30), findViewById(R.id.t31), findViewById(R.id.t32), findViewById(R.id.t33)),
+        )
+        initField()
         // еще 15 строк тут, id у плиток нужно по образцу внести в разметку
     }
 
     fun getCoordFromString(s: String): Coord {
-        // TODO: реализовать функцию, получающую из
-        // строки вида "12" координаты Coord(1,2)
-        // TODO: сообщать в лог координаты
-        return Coord(0,0) // вернуть полученные координаты
+        val x: Int = s[0] - '0'
+        val y: Int = s[1] - '0'
+        Log.i("Coords","${x}, ${y}")
+        return Coord(x,y) // вернуть полученные координаты
     }
     fun changeColor(view: View) {
         val brightColor = resources.getColor(R.color.bright)
         val darkColor = resources.getColor(R.color.dark)
         val drawable = view.background as ColorDrawable
-        if (drawable.color ==brightColor ) {
+        if (drawable.color == brightColor ) {
             view.setBackgroundColor(darkColor)
         } else {
             view.setBackgroundColor(brightColor)
@@ -46,14 +57,39 @@ class MainActivity : AppCompatActivity() {
             changeColor(tiles[coord.x][i])
             changeColor(tiles[i][coord.y])
         }
+        checkVictory()
     }
 
     fun checkVictory() {
+        var countColorTiles: Int = getColorTiles()
+        if (countColorTiles == 0 || countColorTiles == 16){
+            Toast.makeText(this, "You won!", Toast.LENGTH_SHORT).show()
+        }
         // TODO: проверка победы
     }
 
+    fun getColorTiles(): Int {
+        val brightColor = resources.getColor(R.color.bright)
+        var count: Int = 0
+        for (i in 0..3){
+            for (j in 0..3){
+                val drawable = tiles[i][j].background as ColorDrawable
+                if (drawable.color == brightColor){
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
     fun initField() {
-        // TODO: заполнение поля случайными плитками (тёмный/светлый)
+        for (i in 0..3){
+            for (j in 0..3){
+                if (Random.nextBoolean()){
+                    changeColor(tiles[i][j])
+                }
+            }
+        }
     }
 
 
